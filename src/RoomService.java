f318cfdc3419a35e11a25593791dd9cc855a347a
selimzakhama <157222669+selimzakhama@users.noreply.github.com>
2024-01-22 20:40:38 +0200

    Add files via upload

---
+++ src/RoomService.java
@@ -0,0 +1,56 @@
+package com.example.demo.Services;
+
+import com.example.demo.entities.Room;
+import com.example.demo.repositories.RoomRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoomService {
+    private final RoomRepository roomRepository;
+    public RoomService(RoomRepository roomRepository){
+        this.roomRepository = roomRepository;
+    }
+
+    public Room addRoom(String roomName, int roomNumber,String roomType,int roomCapacity,String roomDescription,
+                        String roomImage){
+        if(roomRepository.findRoomByRoomNumber(roomNumber).isEmpty()){
+            Room room = new Room();
+            room.setRoomName(roomName);
+            room.setRoomNumber(roomNumber);
+            room.setRoomCapacity(roomCapacity);
+            room.setRoomType(roomType);
+            room.setRoomDescription(roomDescription);
+            room.setRoomImage(roomImage);
+            return roomRepository.save(room);
+        } else {
+            return null;
+        }
+    }
+    public void saveRoom(Room room){
+        roomRepository.save(room);
+    }
+
+    public List<Room> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    public Room getRoomById(Long id){
+        Optional<Room> roomOptional = roomRepository.findById(id);
+        return roomOptional.orElse(null);
+    }
+
+    public Room deleteRoom(Long id){
+        Optional<Room> roomOptional = roomRepository.findById(id);
+        if (roomOptional.isPresent()) {
+            Room roomToDelete = roomOptional.get();
+            roomRepository.deleteById(id);
+            return roomToDelete;
+        } else {
+            return null;
+        }
+    }
+
+}
